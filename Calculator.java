c28a801c16b226b639f44322990890fd1a051768
Aakrati Barsaiyan <aakratibarsaiyan@gmail.com>
2025-01-07 15:27:23 +0530

    subtraction loaded

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,8 @@
 import java.util.*;
 public class Calculator{
-  
+  public static void subtraction(int a, int b){
+        System.out.println(a-b);
+      }
   
     public static void main(String [] args){
     Scanner s = new Scanner(System.in);
