f42ccc9cc5e62fdeda08a198bf30d76a3d43c4a2
Aakrati Barsaiyan <aakratibarsaiyan@gmail.com>
2025-01-07 15:23:23 +0530

    main loaded

---
+++ Calculator.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Calculator{
+  
+  
+    public static void main(String [] args){
+    Scanner s = new Scanner(System.in);
+       int a  = s.nextInt();
+       int b = s.nextInt();
+       addition(a,b);
+       substraction(a,b);
+       multiplication(a,b);
+       division(a,b);
+       
+ }
+}
